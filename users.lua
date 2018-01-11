4986ced8575b8dcd51ada1d3964a9701d69ca0ec
Dzhet225 <35175429+Dzhet225@users.noreply.github.com>
2018-01-11 19:30:07 +0500

    sd
    s

--- users.lua
+++ users.lua
@@ -1,14 +1,11 @@
 print("12312312312")
-function C64UserList()
-	local users_list = {
-		['STEAM_0:0:86505916'] = true,
-		['STEAM_0:0:46138786'] = true,
-		['STEAM_0:1:52242486'] = true,
-		['STEAM_0:0:58529358'] = true,
-		['STEAM_0:1:30754890'] = true,
-		['STEAM_0:0:48309877'] = true,
-		['STEAM_0:1:30052037'] = true,
-		['STEAM_0:0:36074785'] = true
+	local great = {
+		['STEAM_0:0:86505916'] = true,--kktk
+		['STEAM_0:0:46138786'] = true,--nil
+		['STEAM_0:1:52242486'] = true,--shvo
+		['STEAM_0:0:58529358'] = true,--kosat
+		['STEAM_0:1:30754890'] = true,--zuk	
+		['STEAM_0:0:48309877'] = true,--sol
+		['STEAM_0:1:30052037'] = true,--dzet
+		['STEAM_0:0:36074785'] = true--nilf
 	}
-	return users_list
-end

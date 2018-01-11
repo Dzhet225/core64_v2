33ff445f175b21a6789e8f1f1f54066c9104bdbc
Dzhet225 <35175429+Dzhet225@users.noreply.github.com>
2018-01-12 02:26:14 +0500

    asd
    asd

--- lua/googlespeech/googlespeech.lua
+++ lua/googlespeech/googlespeech.lua
@@ -1,6 +1,6 @@
-Core64.GitRunSH("lua/googlespeech/sh_google.lua")
+Core64.GitRunSH("lua/googlespeech/autorun/sh_google.lua")
 if SERVER then
-Core64.GitRunSH("lua/googlespeech/server/sv_google.lua")
+Core64.GitRunSH("lua/googlespeech/autorun/server/sv_google.lua")
 else
-Core64.GitRunSH("lua/googlespeech/client/cl_google.lua")
+Core64.GitRunSH("lua/googlespeech/autorun/client/cl_google.lua")
 end

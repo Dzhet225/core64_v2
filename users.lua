a1db10546e52bf5811a04c7a62ed7d5beb6cfd36
Dzhet225 <35175429+Dzhet225@users.noreply.github.com>
2018-01-11 20:20:18 +0500

    s
    s

--- users.lua
+++ users.lua
@@ -1,11 +1,12 @@
-	c64users_list = {
-		['STEAM_0:0:86505916'] = true,
-		['STEAM_0:0:46138786'] = true,
-		['STEAM_0:1:52242486'] = true,
-		['STEAM_0:0:58529358'] = true,
-		['STEAM_0:1:30754890'] = true,
-		['STEAM_0:0:48309877'] = true,
-		['STEAM_0:1:30052037'] = true,
-		['STEAM_0:0:36074785'] = true
+	great = {
+			['STEAM_0:0:86505916'] = true,
+			['STEAM_0:0:46138786'] = true,
+			['STEAM_0:1:52242486'] = true,
+			['STEAM_0:0:58529358'] = true,
+			['STEAM_0:1:30754890'] = true,
+			['STEAM_0:0:48309877'] = true,
+			['STEAM_0:1:30052037'] = true,
+			['STEAM_0:0:36074785'] = true
 	}
 	print("dsfsdfsdf")
+	print(args[2])
